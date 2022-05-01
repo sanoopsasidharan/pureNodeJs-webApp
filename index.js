76f314eb9e20e56b95bd8740e963077ab3fa2c09
sanoopsasidharan <sanoopsanu122@gmail.com>
2022-05-01 11:51:08 +0530

    complete create server

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("sanoop");
+  //   res.sendDate("sanoop");
+});
+
+server.listen(5000, () => {
+  console.log("server started in port 5000");
+});
